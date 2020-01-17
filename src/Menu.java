444795763d97d0223b56597a8de66ab4954a3d9e
jamesshep11 <jamesshep11@gmail.com>
2020-01-17 21:54:15 +0200

    Added Menu page

---
+++ src/Menu.java
@@ -0,0 +1,44 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Menu
+{
+    private static DatabaseConnection db;
+    private static Stage stage;
+
+    public Menu() {
+    }
+
+    public void load(DatabaseConnection db) throws IOException {
+        this.db = db;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        stage = new Stage();
+        stage.setTitle("Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void btnMemberDetailsClicked() throws IOException {
+        stage.close();
+        new MemberDetails().load(db);
+    }
+
+    public void btnRegisterClicked() throws IOException, SQLException {
+        stage.close();
+        new Register().load(db);
+    }
+
+    public void btnLogOutClicked() throws IOException {
+        stage.close();
+        new Login().load(db);
+    }
+}
